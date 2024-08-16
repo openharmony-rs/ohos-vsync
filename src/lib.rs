775e908d06d9469a08d70cd972450abd3bcbf5bb
Jonathan Schwender <jonathan.schwender@huawei.com>
2024-08-16 16:55:15 +0800

    cargo fmt
    Signed-off-by: Jonathan Schwender <jonathan.schwender@huawei.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 //! Bindings to the `native_vsync` library on OpenHarmony
-//! 
+//!
 //! This library can be used to receive callbacks on vsync signals
 
 use core::ffi::{c_int, c_void};
 
 use log::debug;
-use ohos_sys::vsync::{OH_NativeVSync, OH_NativeVSync_Create, OH_NativeVSync_Destroy, OH_NativeVSync_FrameCallback, OH_NativeVSync_GetPeriod, OH_NativeVSync_RequestFrame};
+use ohos_sys::vsync::{
+    OH_NativeVSync, OH_NativeVSync_Create, OH_NativeVSync_Destroy, OH_NativeVSync_FrameCallback,
+    OH_NativeVSync_GetPeriod, OH_NativeVSync_RequestFrame,
+};
 
 mod log;
 
@@ -13,45 +16,39 @@ pub struct NativeVsync {
     raw: *mut OH_NativeVSync,
 }
 
-
 #[derive(Debug)]
 pub enum NativeVsyncError {
     InvalidArgs,
     CreateFailed,
-    RawErr(c_int)
+    RawErr(c_int),
 }
 
 impl NativeVsync {
     pub fn new(name: &str) -> Result<Self, NativeVsyncError> {
-        let name_len: u32 = name.len().try_into().map_err(|_| NativeVsyncError::InvalidArgs)?;
+        let name_len: u32 = name
+            .len()
+            .try_into()
+            .map_err(|_| NativeVsyncError::InvalidArgs)?;
         // SAFETY: OH_NativeVSync_Create will not attempt to modify the string.
-        // The official example also shows usage of a name without a trailing `\0`  
-        let raw = unsafe {
-            OH_NativeVSync_Create(name.as_ptr().cast() , name_len)
-        };
-        Ok(
-            NativeVsync {
-                raw,
-            }
-        )
+        // The official example also shows usage of a name without a trailing `\0`
+        let raw = unsafe { OH_NativeVSync_Create(name.as_ptr().cast(), name_len) };
+        Ok(NativeVsync { raw })
     }
 
     /// Create from a raw pointer to a valid `OH_NativeVSync` instance.
-    /// 
+    ///
     /// # Safety
-    /// 
-    /// `native_vsync` must be a valid, live OH_NativeVSync instance. 
+    ///
+    /// `native_vsync` must be a valid, live OH_NativeVSync instance.
     /// The ownership of `native_vsync` must be exclusive and is transferred to the new object.
     pub unsafe fn from_raw(native_vsync: *mut OH_NativeVSync) -> Self {
         debug_assert!(!native_vsync.is_null());
         debug_assert!(native_vsync.is_aligned());
-        Self {
-            raw: native_vsync
-        }
+        Self { raw: native_vsync }
     }
 
     /// Returns the refernece to the raw OH_NativeVSync and consumes self
-    /// 
+    ///
     /// `NativeVsync::from_raw` can be used to reconstruct Self later.
     /// This can be used to pass the owned NativeVsync object to the callback function.
     pub fn into_raw(self) -> *mut OH_NativeVSync {
@@ -61,17 +58,19 @@ impl NativeVsync {
     }
 
     /// Request a Callback to `callback` on the next Vsync frame
-    /// 
+    ///
     /// `data` will be passed to the callback.
-    /// 
+    ///
     /// # Safety
-    /// 
-    /// If data is used in the callback then data must live long enough and be ThreadSafe to use. 
+    ///
+    /// If data is used in the callback then data must live long enough and be ThreadSafe to use.
     /// Todo: Define the requirements more clearly.
-    pub unsafe fn request_raw_callback(&self, callback: OH_NativeVSync_FrameCallback, data: *mut c_void) -> Result<(), NativeVsyncError> {
-        let res = unsafe {
-            OH_NativeVSync_RequestFrame(self.raw, callback, data)
-        };
+    pub unsafe fn request_raw_callback(
+        &self,
+        callback: OH_NativeVSync_FrameCallback,
+        data: *mut c_void,
+    ) -> Result<(), NativeVsyncError> {
+        let res = unsafe { OH_NativeVSync_RequestFrame(self.raw, callback, data) };
         if res == 0 {
             Ok(())
         } else {
@@ -79,10 +78,12 @@ impl NativeVsync {
         }
     }
 
-    pub unsafe fn request_raw_callback_with_self(self, callback: OH_NativeVSync_FrameCallback) -> Result<(), NativeVsyncError> {
-        let res = unsafe {
-            OH_NativeVSync_RequestFrame(self.raw, callback, self.raw as *mut c_void)
-        };
+    pub unsafe fn request_raw_callback_with_self(
+        self,
+        callback: OH_NativeVSync_FrameCallback,
+    ) -> Result<(), NativeVsyncError> {
+        let res =
+            unsafe { OH_NativeVSync_RequestFrame(self.raw, callback, self.raw as *mut c_void) };
         if res == 0 {
             core::mem::forget(self);
             Ok(())
@@ -92,9 +93,8 @@ impl NativeVsync {
         }
     }
 
-
     /// Returns the vsync period in nanoseconds.
-    pub fn get_period(&self) -> Result<u64, NativeVsyncError>  {
+    pub fn get_period(&self) -> Result<u64, NativeVsyncError> {
         let period = unsafe {
             let mut period: i64 = -1;
             let res = OH_NativeVSync_GetPeriod(self.raw, (&mut period) as *mut i64);
@@ -112,11 +112,8 @@ impl NativeVsync {
 
 impl Drop for NativeVsync {
     fn drop(&mut self) {
-        // SAFETY: We never leaked the pointer, so we are sure we still own the 
+        // SAFETY: We never leaked the pointer, so we are sure we still own the
         // nativeVsync object and can destroy it.
-        unsafe {
-            OH_NativeVSync_Destroy(self.raw)
-        };
-
+        unsafe { OH_NativeVSync_Destroy(self.raw) };
     }
 }
